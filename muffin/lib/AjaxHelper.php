66a56486da49ab1f9c903c273502c2bce2abaaf7
tcaorsi <lucifers.preacher@gmail.com>
2010-01-14 15:31:55 -0200

    Added ajax helper to better handle strings

---
+++ muffin/lib/AjaxHelper.php
@@ -0,0 +1,32 @@
+<?php
+class AjaxHelper extends Helper
+{
+	public function html2UnicodeJsString($string)
+	{
+		$in = array("&aacute;", "&eacute;", "&iacute;", "&oacute;", "&uacute;", "&ntilde;",
+					"&Aacute;", "&Eacute;", "&Iacute;", "&Oacute;", "&Uacute;", "&Ntilde;");
+
+		$out = array("\u00e1", "\u00e9", "\u00ed", "\u00f3", "\u00fa", "\u00f1",
+					 "\u00c1", "\u00c9", "\u00cd", "\u00d3", "\u00da", "\u00d1");
+		
+		return str_replace($in, $out, $string);
+	}
+	
+	public function getJson($obj)
+	{
+		if(is_array($obj))
+		{
+			foreach($obj as $key => $value)
+			{
+				$obj[$key] = $this->html2UnicodeJsString($value);
+			}
+		}
+		else
+		{
+			$obj = $this->html2UnicodeJsString($obj);
+		}
+		
+		return str_replace("\\\\", "\\", json_encode($obj));
+	}
+}
+?>
